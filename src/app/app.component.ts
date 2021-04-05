7442c4ec8ed7a4ea5e742abd62ff328ea1f8f70c
Habeebrahman98 <habeebrhman46@gmail.com>
2021-04-05 22:41:31 +0530

    webapp

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'first-site';
+  myimage:string ="assets/images/cr2.jpg";
+  myimage1:string ="assets/images/cw1.jpg";
+  myimage2:string ="assets/images/cw2.jpg";
+  myimage3:string ="assets/images/cw3.jfif";
+  myimage4:string ="assets/images/cw4.jfif";
+  myimage5:string ="assets/images/p1.jpg";
+  myimage6:string ="assets/images/p2.png";
+  myimage7:string ="assets/images/p3.png";
+  myimage8:string ="assets/images/p4.png";
+  myimage9:string ="assets/images/p5.png";
+  myimage10:string ="assets/images/p6.png";
+  myimage11:string ="assets/images/cr3.jpg";
+  myimage12:string ="assets/images/cl.png";
+  myimage13:string ="assets/images/mn.png";
+  myimage14:string ="assets/images/ll.png";
+  myimage15:string ="assets/images/cl.png";
+  myimage16:string ="assets/images/ml.png";
+  myimage17:string ="assets/images/mn.png";
+  myimage18:string ="assets/images/ll.png";
+  myimage19:string ="assets/images/cl.png";
+  myimage20:string ="assets/images/ml.png";
+}
